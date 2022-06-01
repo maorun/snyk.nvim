c1e1f7bb20f95a42757c1919aa9285e92149bcb9
Marco Driemel <marco.driemel@axelspringer.com>
2022-06-01 09:03:21 +0200

    add check if no results found

--- lua/maorun/snyk/init.lua
+++ lua/maorun/snyk/init.lua
@@ -5,6 +5,11 @@ local Job = require('plenary.job')
 
 local namespace = vim.api.nvim_create_namespace('snyk')
 
+local function printWithoutHistory(text)
+    text = 'Snyk: ' .. text
+    vim.cmd("echo '" .. text .. "'")
+end
+
 -- perform diagnostic
 local function test_code(currentFile, fullFile, json)
     diagnostics = vim.tbl_map(function(result)
@@ -43,11 +48,14 @@ end
 local function startJob(params)
     local cmd = "snyk code test --json"
     local check = {}
-    local fullFile = vim.fn.expand('<afile>')
-    local file = vim.fn.expand('<afile>:t')
+    local fullFile = params.fullFile
+    -- @todo extract file and cwd from fullFile
+    local file = params.file
+    local cwd = params.cwd
+    printWithoutHistory('startJob')
     Job:new({
         command = snykCommand,
-        cwd = vim.fn.expand('<afile>:p:h'),
+        cwd = cwd,
         args = {
             "code",
             "test",
@@ -62,18 +70,19 @@ local function startJob(params)
         end,
         on_exit = function(signal)
             vim.schedule(function()
-                local json = vim.fn.json_decode(check)
-                test_code(file, fullFile, json)
+                if (check[1] == '{') then
+                    local json = vim.fn.json_decode(check)
+                    test_code(file, fullFile, json)
+                else
+                    printWithoutHistory('got an error: "' .. check[1] .. '"')
+                end
             end)
         end,
     }):start()
 end
 
-local function printWithoutHistory(text)
-    vim.cmd("echo '" .. text .. "'")
-end
 
-local function checkSnykAvailable()
+local function checkSnykAvailable(afterCheck)
     if (vim.fn.executable(snykCommandGlobal) == 0) then
         if (vim.fn.executable(snykCommand) == 0 and vim.fn.executable('npm') == 0) then
             printWithoutHistory("Snyk not available")
@@ -85,20 +94,67 @@ local function checkSnykAvailable()
                     "install",
                 },
                 interactive = false,
+                on_exit = function(signal)
+                    vim.schedule(function()
+                        afterCheck()
+                    end)
+                end,
             }):start()
             printWithoutHistory('Snyk installed')
         else
             printWithoutHistory("Snyk available (plugin)")
+            afterCheck()
         end
     else
         snykCommand = snykCommandGlobal
         printWithoutHistory('Snyk available')
+        afterCheck()
     end
+end
+
+local function auth()
+    local apiKey = ''
+    Job:new({
+        command = snykCommand,
+        cwd = rootDir,
+        args = {
+            "config",
+            "get",
+            "api",
+        },
+        interactive = false,
+        on_stdout = function(error, data)
+            apiKey = data
+        end,
+        on_exit = function(signal)
+            vim.schedule(function()
+                if (apiKey == '') then
+                    printWithoutHistory('Snyk not yet authenticated')
+                    Job:new({
+                        command = snykCommand,
+                        args = {
+                            "auth",
+                        },
+                        interactive = false,
+                        on_exit = function(signal)
+                            vim.schedule(function()
+                                printWithoutHistory('Snyk now authenticated')
+                            end)
+                        end
+                    }):start()
+                else
+                    printWithoutHistory('Snyk was authenticated')
+                end
+            end)
+        end,
+    }):start()
 end
 
 local M = {}
 function M.setup(options)
-    checkSnykAvailable()
+    checkSnykAvailable(function()
+        auth()
+    end)
 
     local snykGroup = vim.api.nvim_create_augroup('snyk', {})
     vim.api.nvim_create_autocmd({"BufReadPost", "BufWritePost" }, {
@@ -107,9 +163,16 @@ function M.setup(options)
             '*.c', '*.cc', '*.cpp', '*.cxx', '*.h', '*.hpp', '*.hxx', '*.ejs', '*.es', '*.es6', '*.htm', '*.html', '*.js', '*.jsx', '*.ts', '*.tsx', '*.vue', '*.java', '*.erb', '*.haml', '*.rb', '*.rhtml', '*.slim', '*.py', '*.go', '*.ASPX', '*.Aspx', '*.CS', '*.Cs', '*.aspx', '*.cs', '*.php', '*.xml'
         },
         callback = function(params)
-            startJob(params)
+            startJob({
+                fullFile = vim.fn.expand('<afile>'),
+                file = vim.fn.expand('<afile>:t'),
+                cwd = vim.fn.expand('<afile>:p:h'),
+            })
         end
     })
 end
+
+M.auth = auth
+M.testCode = startJob
 
 return M
